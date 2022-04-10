86b57773017d78d908598c35e7e91c65da5535f6
edgvac2298 <edgardovac2298@gmail.com>
2022-04-10 13:29:38 -0500

    Return quantity of characters

--- lab3/index.js
+++ lab3/index.js
@@ -22,3 +22,33 @@ const number = prompt('Coloca tu número: ');
 const value = checkPalindrome(number);
 
 console.log(value);
+
+
+// Función que retorna cantidad de caractéres de una cadena
+
+function checkTheString(str){
+
+var counts = {};
+
+// Declaración de variables
+var ch, index, len, count;
+
+// Loop dependiendo la cantidad de string
+for (index = 0, len = str.length; index < len; ++index) {
+    // obtener los caracteres 
+    ch = str.charAt(index); 
+    count = counts[ch];
+    counts[ch] = count ? count + 1 : 1;       
+}
+    for (ch in counts) {
+        alert(ch + " count: " + counts[ch]);
+    }
+}
+
+// Mensaje que le indica al usuario la cadena a colocar
+const string = prompt('Coloque su cadena: ');
+
+// Llama a la función mandando el número
+const value_string = checkTheString(string);
+
+console.log(value_string);
