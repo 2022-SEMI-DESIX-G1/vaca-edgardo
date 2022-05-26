9b43ac7cf89d597f677ad1a537f1ae81a6db74c7
edgvac2298 <edgardovac2298@gmail.com>
2022-05-26 10:02:06 -0500

    add evolution of pokemon

--- lab7/index.js
+++ lab7/index.js
@@ -1,27 +1,54 @@
 const utils = require("./utils");
+const axios = require("axios").default
+
+var link_pokeapi = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/pokemon/'
+  });
+
 (async () => {
-    const pokemonName = "Pikachu"
-
+    const pokemonName = "Pikachu" 
+  
     try {
-
         const { id, name, weight,
             height, abilities,
-            evolutionChain } = await utils.getPokemon({ name: pokemonName.toLowerCase() });
+            evolutionChain } = result = await utils.getPokemon({ name: pokemonName.toLowerCase() });
+            
+            link_pokeapi.get(result.name).then(function (res) { 
+                var pokemonSpecies = axios.create({
+                    baseURL: res.data.species.url
+                });
+                
+                pokemonSpecies.get().then(function (pokeResponse) { 
+                    var pokeEvolution = axios.create({
+                    baseURL: pokeResponse.data.evolution_chain.url
+                });
 
+                pokeEvolution.get().then(function (evolutionResponse) { 
+                    let chainEvolution = [];
+                    let evolutionChain  = [[evolutionResponse.data.chain.species.name]];
+                    let evolutionsOfPokemons = [evolutionResponse.data.chain.evolves_to];
+                    
+                    while (evolutionsOfPokemons[0].length !== 0 && evolutionsOfPokemons[0] !== undefined) {
+                        chainEvolution = evolutionsOfPokemons.shift();
+                        
+                        for (const [i] of chainEvolution.entries()) {
+                            evolutionChain.push(chainEvolution[i].species.name);
+                            evolutionsOfPokemons.push(chainEvolution[i].evolves_to);
+                        }
+                    }
+                    console.log({evolutionChain});
+                })
+            })
+        });
         console.log({
-            id,
-            name,
-            weight,
-            height,
+            name: result.name,
+            id: result.id,
+            weight: result.weight,
+            height: result.height,
             abilities: abilities.map((component) => {
                 return  `${component.ability.name[0].toUpperCase()}${component.ability.name.substring(1)}`
-            }), 
-            
-            /*evolutionChain: evolutionChain.map((pokemon) => {
-                return { name: utils.capitalize(pokemon.name), isBaby: pokemon.isBaby ? "is baby" : "not" }
-            })*/
-        })
-
+            })
+        });
     } catch (e) {
         console.log(e)
     }
