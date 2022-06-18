98018862093c39d2b154ec6fbb9055929af0e8b3
edgvac2298 <edgardovac2298@gmail.com>
2022-06-18 18:37:02 -0500

    Backend works

--- proyecto1/backend/app.js
+++ proyecto1/backend/app.js
@@ -18,7 +18,6 @@ app.use(cors());
 app.get("/cache", function (req, res) {
     res.json({ data: CACHE });
   });
-  
   app.post("/pokemon/:name", async function (req, res) {
     const { name } = req.params;
     if (CACHE[name]) {
@@ -27,7 +26,6 @@ app.get("/cache", function (req, res) {
     if (ERROR[name]) {
       return res.json({ name, data: JSON.parse(ERROR[name]), isCached: true });
     }
-  
     const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
     let responseData;
     try {
@@ -39,6 +37,33 @@ app.get("/cache", function (req, res) {
       ERROR[name] = JSON.stringify({ name, error: "Invalid pokemon." });
     }
     res.json({ name, data: responseData, isCached: false });
+  });
+
+  app.get("/encounters/:name", async function (req, res) {
+    const name = req.params.name;
+    const link = `https://pokeapi.co/api/v2/pokemon/`;
+    var contentEncounters = [];
+    try {
+      const url = link+`${name}`;
+      if (LOCATION_CACHE[name]) {
+        res.json({data: JSON.parse(LOCATION_CACHE[name]), isCached: true});
+      } else {
+        let urlLocationResponse = await urlResponse(url);
+        const locationAreaEncounters = await urlResponse(urlLocationResponse.location_area_encounters);
+        
+        for (var i = 0; i < locationAreaEncounters.length; i++) {
+          nameEncounter = "Name: "+locationAreaEncounters[i].location_area.name;
+          chanceEncounter = "Chance: "+locationAreaEncounters[i].version_details[0].encounter_details[0].chance+"%";
+          methodEncounter = "Method: "+locationAreaEncounters[i].version_details[0].encounter_details[0].method.name;
+          versionEncounter = "Version: "+locationAreaEncounters[i].version_details[0].version.name;
+          contentEncounters.push({name: nameEncounter, chance: chanceEncounter, method: methodEncounter, version: versionEncounter});
+        }
+      }
+      res.json({data: contentEncounters, isCached: false});
+      LOCATION_CACHE[name] = JSON.stringify(contentEncounters);
+    } catch {
+      res.json({data: 'Invalid Pokemon'});
+    }
   });
 
   app.get("/evolution/:name", async function (req, res) {
@@ -55,12 +80,8 @@ app.get("/cache", function (req, res) {
         const evolution_chain = await urlResponse(urlInfoResponse.species.url);
         const chain = await urlResponse(evolution_chain.evolution_chain.url);
         let allEvolution = [chain.chain.evolves_to];
-        
         let speciesName = chain.chain.species.name;
-        
-
         const urlSpeciesName = link+`${speciesName}`;
-
         let responseInfo = await urlResponse(urlSpeciesName);
         arrayEvolves.push({name: responseInfo.name});
               
@@ -68,32 +89,28 @@ app.get("/cache", function (req, res) {
           chainEvolution = allEvolution.shift();
           for (const [i] of chainEvolution.entries()) {
             allEvolution.push(chainEvolution[i].evolves_to);
-    
                 let namesEvolution = chainEvolution[i].species.name;
                 const evolutionUrl = link+`${namesEvolution}`;
-                
                 let responseDataInfo = await urlResponse(evolutionUrl);
                 arrayEvolves.push({name: responseDataInfo.name});
+              }
+            }
+          }
+          res.json({data: arrayEvolves, isCached: false});
+          EVOLUTION_CACHE[name] = JSON.stringify(arrayEvolves);
+        } catch {
+          res.json({data: 'Invalid Pokemon'});
         }
+      });
+      async function urlResponse(url) {
+        try {
+          const { data } = await axios.get(url);
+          response = data;
+        } catch {
+          response = 'Invalid Pokemon';
         }
+        return response;
       }
-        res.json({data: arrayEvolves, isCached: false});
-        EVOLUTION_CACHE[name] = JSON.stringify(arrayEvolves);
-    } catch {
-      res.json({data: 'Invalid Pokemon'});
-    }
-  });
-
-  async function urlResponse(url) {
-    try {
-      const { data } = await axios.get(url);
-      response = data;
-    } catch {
-      response = 'Invalid Pokemon';
-    }
-    return response;
-  }
-  
-  app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}...`);
-  });
+      app.listen(PORT, () => {
+      console.log(`Running on port ${PORT}...`);
+    });
